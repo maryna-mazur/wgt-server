b768e337a8af445e3036906e62553312d8f096ed
Maryna Mazur <marinemazur@gmail.com>
2026-05-06 16:49:58 +0300

    Serve full buildResult as static files

---
+++ server.js
@@ -0,0 +1,47 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+const ROOT = __dirname;
+
+const MIME_TYPES = {
+  ".html": "text/html",
+  ".js": "application/javascript",
+  ".css": "text/css",
+  ".json": "application/json",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".svg": "image/svg+xml",
+  ".xml": "application/xml",
+  ".wgt": "application/octet-stream",
+  ".woff": "font/woff",
+  ".woff2": "font/woff2",
+  ".ttf": "font/ttf",
+};
+
+const server = http.createServer((req, res) => {
+  const url = decodeURIComponent(req.url.split("?")[0]);
+  const filePath = path.join(ROOT, url === "/" ? "index.html" : url);
+
+  if (!fs.existsSync(filePath) || !fs.statSync(filePath).isFile()) {
+    res.writeHead(404);
+    res.end("Not found");
+    return;
+  }
+
+  const ext = path.extname(filePath).toLowerCase();
+  const contentType = MIME_TYPES[ext] || "application/octet-stream";
+  const stat = fs.statSync(filePath);
+
+  res.writeHead(200, {
+    "Content-Type": contentType,
+    "Content-Length": stat.size,
+  });
+
+  fs.createReadStream(filePath).pipe(res);
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Static server running on port ${PORT}`);
+});
